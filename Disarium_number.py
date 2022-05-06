6285e39a7cfda0ef802b40d0b1bc9c3d96c4fd25
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-05-06 20:36:22 +0530

    n=int(input()) length=len(str(n)) c=0 Sum=0 temp=n while(n):     rem=n%10     Sum=Sum+int(rem**length)     n=n//10     length=length-1 if Sum==temp :     print(True) else :     print(False)

---
+++ Disarium_number.py
@@ -0,0 +1,14 @@
+n=int(input())
+length=len(str(n))
+c=0
+Sum=0
+temp=n
+while(n):
+    rem=n%10
+    Sum=Sum+int(rem**length)
+    n=n//10
+    length=length-1
+if Sum==temp :
+    print(True)
+else :
+    print(False)
