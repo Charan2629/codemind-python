8b549a1cce3adcd9f0b0706eda4d1cc4b27a5bc8
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-05-23 21:25:00 +0530

    def sum_of_digits(n):     s=0     while n:         rem=n%10         s+=rem         n//=10     if s>9:         return sum_of_digits(s)     return s n=int(input()) print(sum_of_digits(n))

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,14 +1,11 @@
 def sum_of_digits(n):
-    s = 0
-
+    s=0
     while n:
-        s += n % 10
-        n //= 10
-
-    if s > 9:
+        rem=n%10
+        s+=rem
+        n//=10
+    if s>9:
         return sum_of_digits(s)
-
     return s
-
-n = int(input())
+n=int(input())
 print(sum_of_digits(n))
