c159be42da753732704fdbe414e8849968fd1800
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-06-21 20:55:54 +0530

    n=int(input()) for i in range(n+1):     c=0     for j in range(1,i+1):         if(i%j==0):             c+=1     if(c==2):         a=i i=n while(1):     c=0     for j in range(1,i+1):         if(i%j==0):             c+=1     i+=1     if(c==2):         b=j         break x=abs(n-a) y=abs(n-b) if(x<y):     print(x) else:     print(y)

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -1,23 +1,24 @@
 n=int(input())
-for i in range (n,1,-1):
-        for j in range(2,int(i**0.5)+1):
-            if i%j==0:
-                break
-        else :
-            temp=i
-            break
-b=n
-while True:
-        for j in range(2,int(n**0.5)+1):
-            if n%j==0:
-                break
-        else :
-            temp1=n
-            break
-        n+=1
-res=abs(b-temp)
-res1=abs(b-temp1)
-if res<=res1 :
-        print(res)
-else :
-        print(res1)
+for i in range(n+1):
+    c=0
+    for j in range(1,i+1):
+        if(i%j==0):
+            c+=1
+    if(c==2):
+        a=i
+i=n
+while(1):
+    c=0
+    for j in range(1,i+1):
+        if(i%j==0):
+            c+=1
+    i+=1
+    if(c==2):
+        b=j
+        break
+x=abs(n-a)
+y=abs(n-b)
+if(x<y):
+    print(x)
+else:
+    print(y)
