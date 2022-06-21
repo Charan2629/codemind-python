a6e5da576a464328e19896fbb646f7d6816e63e7
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-06-21 20:54:30 +0530

    def prime(n):     c=0     for i in range(1,n+1):         if(n%i==0):             c+=1     if(c==2):         return 1     else:         return 0 h=int(input()) while(h):     n=int(input())     for i in range(1,n+1):         if(prime(i)):             a=i     i=n+1     while(1):         if(prime(i)):             b=i             break         i+=1     x=abs(a-n)     y=abs(b-n)     if(x<y):         print(a)     elif(x==y):         print(a)     else:         print(b)     h-=1

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,23 +1,30 @@
-n=int(input())
-for i in range(1,n+1,1):
-    a=int(input())
-    b=a
-    for i in range(a,1,-1):
-        for j in range(2,i,1):
-            if i%j==0:
-                break
-        else:
-            c=i
-            break
-    while a:
-        for j in range(2,a,1):
-            if a%j==0:
-                break
-        else:
-            d=a
-            break
-        a+=1
-    if b-c<=d-b:
-        print(c)
+def prime(n):
+    c=0
+    for i in range(1,n+1):
+        if(n%i==0):
+            c+=1
+    if(c==2):
+        return 1
     else:
-        print(d)
+        return 0
+h=int(input())
+while(h):
+    n=int(input())
+    for i in range(1,n+1):
+        if(prime(i)):
+            a=i
+    i=n+1
+    while(1):
+        if(prime(i)):
+            b=i
+            break
+        i+=1
+    x=abs(a-n)
+    y=abs(b-n)
+    if(x<y):
+        print(a)
+    elif(x==y):
+        print(a)
+    else:
+        print(b)
+    h-=1
