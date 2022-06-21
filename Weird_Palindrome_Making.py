a992bace0723983465a1d9f6b922a41fc3afae06
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-06-21 21:44:39 +0530

    T=int(input()) for i in range(0,T):     n=int(input())     a=[int(x) for x in input().split()]     if n==1:         print(0)     else:         v=[]         for i in a:             if i in range(1,100000000,2):                 v.append(i)         if len(v)==1:             print(0)         elif len(v)%2==0:             print(int(len(v)/2))         else:             print(int((len(v)-1)/2))

---
+++ Weird_Palindrome_Making.py
@@ -0,0 +1,17 @@
+T=int(input())
+for i in range(0,T):
+    n=int(input())
+    a=[int(x) for x in input().split()]
+    if n==1:
+        print(0)
+    else:
+        v=[]
+        for i in a:
+            if i in range(1,100000000,2):
+                v.append(i)
+        if len(v)==1:
+            print(0)
+        elif len(v)%2==0:
+            print(int(len(v)/2))
+        else:
+            print(int((len(v)-1)/2))
