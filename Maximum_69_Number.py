2da1b691803a42199840b56ec502be91670ddd86
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-06-21 20:58:46 +0530

    a=int(input()) s=list(str(a)) g=0 for i in range(len(s)):     if(s[i]=='6'):         s[i]='9'         break     else:         continue f=''.join(s) print(f)

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -1,4 +1,11 @@
-def maximum69Number(num):
-    return(str(num).replace('6', '9', 1))
-num=int(input())
-print(maximum69Number(num))
+a=int(input())
+s=list(str(a))
+g=0
+for i in range(len(s)):
+    if(s[i]=='6'):
+        s[i]='9'
+        break
+    else:
+        continue
+f=''.join(s)
+print(f)
