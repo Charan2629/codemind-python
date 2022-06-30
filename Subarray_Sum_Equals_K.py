2c94a24ea905a64c2e1ec00f44f65467e8305e88
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-06-30 11:53:12 +0530

    t,k=map(int,input().split()) arr = list(map(int,input().split())) n = len(arr)
    res = 0
    
    for i in range(n):
    	sum = 0  
    	for j in range(i, n):
     		sum += arr[j]
     		
     		if sum == k:
      			res += 1
    
    print(res)

---
+++ Subarray_Sum_Equals_K.py
@@ -0,0 +1,15 @@
+t,k=map(int,input().split())
+arr = list(map(int,input().split()))
+n = len(arr)
+
+res = 0
+
+for i in range(n):
+	sum = 0  
+	for j in range(i, n):
+ 		sum += arr[j]
+ 		
+ 		if sum == k:
+  			res += 1
+
+print(res)
