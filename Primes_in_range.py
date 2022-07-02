98dfd9be6fe1295cf237aed20170e2eeeb92adee
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-07-02 10:57:09 +0530

    def is_prime(n):     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     else:         return 1 s=int(input()) e=int(input()) c=0 for i in range(s,e+1):     if i>1:         if is_prime(i):             c+=1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,18 +1,14 @@
 def is_prime(n):
-    for i in range(2,int(n**0.5)+1) :
-        if n%i==0 :
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
             return 0
     else:
         return 1
 s=int(input())
 e=int(input())
 c=0
-if s>1:
-    for i in range(s,e+1) :
-        if(is_prime(i)):
-            c+=1
-else :
-    for i in range(s+1,e+1) :
-        if(is_prime(i)):
+for i in range(s,e+1):
+    if i>1:
+        if is_prime(i):
             c+=1
 print(c)
