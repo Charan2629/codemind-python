5c50527c64cf138b3b4e3e7a7e75551ce02c92d9
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-07-21 19:49:14 +0530

    n , m = map(int,input().split()) a = list(map(int,input().split())) b = list(map(int,input().split())) c = list(set(a)) d = list(set(b)) count = 0 for i in c:     if i not in d:         count+=1 for i in d:     if i not in c:         count+=1 print(count)

--- Count_elements_II.py
+++ Count_elements_II.py
@@ -1,14 +1,13 @@
-p,q=map(int,input().split())
-a=list(map(int,input().split()))
-b=list(map(int,input().split()))
-k=0
-s=0
-c=set(a)
-d=list(set(b))
-for i in d:
-    if i not in c:
-        s+=1
+n , m = map(int,input().split())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+c = list(set(a))
+d = list(set(b))
+count = 0
 for i in c:
     if i not in d:
-        k+=1
-print(s+k)
+        count+=1
+for i in d:
+    if i not in c:
+        count+=1
+print(count)
