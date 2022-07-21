236ab6505f1fc02d491d30e52243b48b12c4cae3
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-07-21 20:07:29 +0530

    def rev(a):     s=0     while a>0:         rem=a%10         s=(s*10)+rem         a//=10     return s a=int(input()) p1=0 p2=0 d1=0 d2=0 for i in range(a-1,0,-1):     if rev(i)==i:         p1=i         d1=a-i         break for i in range(a+1,10000):     if rev(i)==i:         p2=i         d2=i-a         break if d1==d2:     print(p1,p2) elif d1>d2:     print(p2) elif d1<d2:     print(p1)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,22 +1,28 @@
-def palin(i):
-    temp=i
-    r=0
-    while(i):
-        rem=i%10
-        r=r*10+rem
-        i//=10
-    if r==temp:
-        return 1
-n=int(input())
-for i in range(n-1,0,-1):
-    if palin(i):
-        k=i
+def rev(a):
+    s=0
+    while a>0:
+        rem=a%10
+        s=(s*10)+rem
+        a//=10
+    return s
+a=int(input())
+p1=0
+p2=0
+d1=0
+d2=0
+for i in range(a-1,0,-1):
+    if rev(i)==i:
+        p1=i
+        d1=a-i
         break
-for j in range(n+1,100000):
-    if palin(j):
-        t=j
+for i in range(a+1,10000):
+    if rev(i)==i:
+        p2=i
+        d2=i-a
         break
-if t-n==n-k:
-    print(k,t)
-else:
-    print(k)
+if d1==d2:
+    print(p1,p2)
+elif d1>d2:
+    print(p2)
+elif d1<d2:
+    print(p1)
