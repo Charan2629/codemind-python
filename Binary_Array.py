4ec140ce24469d10ef683d90c81f5f3d18be51cb
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-07-21 20:41:50 +0530

    n=int(input()) l=list(map(int,input().split())) for i in l:     if i!=0 and i!=1:         print(False)         break else:     print(True)

--- Binary_Array.py
+++ Binary_Array.py
@@ -1,10 +1,8 @@
 n=int(input())
 l=list(map(int,input().split()))
-c=0
 for i in l:
-    if i==0 or i==1:
-        c+=1
-if c==n:
-    print(True)
+    if i!=0 and i!=1:
+        print(False)
+        break
 else:
-    print(False)
+    print(True)
