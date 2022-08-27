cbed08ec7aac4353ca49593fed7ce8711b5fff9c
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-08-27 21:08:23 +0530

    a,b=map(int,input().split()) s=max(a,b) while(True):     if s%a==0 and s%b==0:         print(s)         break     s+=1

--- LCM.py
+++ LCM.py
@@ -1,11 +1,7 @@
-def lcm(a,b) :
-    if a>b:
-        a,b=b,a
-    c=b
-    while True :
-        if c%a==0 and c%b==0 :
-            return c
-            break
-        c+=1
 a,b=map(int,input().split())
-print(lcm(a,b))
+s=max(a,b)
+while(True):
+    if s%a==0 and s%b==0:
+        print(s)
+        break
+    s+=1
