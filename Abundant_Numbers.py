926ec7a8c759c949b8e59f9dc3c9cf9db6cbaf9e
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-08-30 12:15:01 +0530

    n=int(input()) Sum=0 for i in range(1,n//2+1):     if n%i==0:         Sum+=i if Sum>n:     print(True) else:     print(False)

--- Abundant_Numbers.py
+++ Abundant_Numbers.py
@@ -1,10 +1,9 @@
 n=int(input())
 Sum=0
-temp=n
-for i in range (1,n//2+1) :
-    if n%i==0 :
-        Sum=Sum+i
-if Sum > temp :
+for i in range(1,n//2+1):
+    if n%i==0:
+        Sum+=i
+if Sum>n:
     print(True)
-else :
+else:
     print(False)
