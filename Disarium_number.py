6bfe14d6a872189b224e3663675a62194be03e08
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-08-30 12:10:27 +0530

    n=int(input()) Sum,p=0,1 for i in str(n):     Sum+=int(i)**p     p+=1 if Sum==n:     print(True) else:     print(False)

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,14 +1,9 @@
 n=int(input())
-length=len(str(n))
-c=0
-Sum=0
-temp=n
-while(n):
-    rem=n%10
-    Sum=Sum+int(rem**length)
-    n=n//10
-    length-=1
-if Sum==temp :
+Sum,p=0,1
+for i in str(n):
+    Sum+=int(i)**p
+    p+=1
+if Sum==n:
     print(True)
-else :
+else:
     print(False)
