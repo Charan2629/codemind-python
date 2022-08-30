8e7e0e51fb0f8644b0db96e2ea5571764017c3ab
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-08-30 12:21:00 +0530

    n=int(input()) a,b=0,1 print(0,end=' ') for i in range(n-1):     res=a+b     print(res,end=' ')     res,b,a=b,a,res

--- fibonacci.py
+++ fibonacci.py
@@ -1,14 +1,7 @@
 n=int(input())
-b=1
-a=0
-c=a+b
-if n==1 :
-    print(a)
-else :
-    print(a,end=" ")
-    print(b,end=" ")
-    for i in range(2,n) :
-        c=a+b
-        a=b
-        b=c
-        print(c,end=" ")
+a,b=0,1
+print(0,end=' ')
+for i in range(n-1):
+    res=a+b
+    print(res,end=' ')
+    res,b,a=b,a,res
