957cbddcc24d9daf10622a3674a05b2694dfd92f
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-08-31 14:45:08 +0530

    n=int(input()) l=list(map(int,input().split())) a,b=map(int,input().split()) k=[] flag=0 for i in l:     if i<a or i>b:         k.append(i)         flag=1 if flag:     print(*k) else:     print(-1)

--- elements_not_in_between_A_and_B.py
+++ elements_not_in_between_A_and_B.py
@@ -1,17 +1,13 @@
 n=int(input())
 l=list(map(int,input().split()))
-s,e=map(int,input().split())
-Sum=0
+a,b=map(int,input().split())
+k=[]
 flag=0
-a=[]
-b=[]
-for x in range(s,e+1):
-    a.append(x)
 for i in l:
-    if i not in a:
-        b.append(i)
+    if i<a or i>b:
+        k.append(i)
         flag=1
-if flag==1:
-    print(*b)
+if flag:
+    print(*k)
 else:
     print(-1)
