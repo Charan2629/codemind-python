1acd2a8946518b6a39a3c6cc403a24f101f4a28a
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-09-06 12:04:21 +0530

    n=int(input()) s=n*n st=str(n) sq=int(st[::-1])**2 ns=str(sq) if str(s)[::-1]==ns:     print(True) else:     print(False)

--- Adam_number.py
+++ Adam_number.py
@@ -1,22 +1,9 @@
 n=int(input())
-r=0
-rev=0
-rev2=0
-sq=n*n
-
-while n > 0:
-    rem=n%10
-    rev=rev*10+rem
-    n=n//10
-    
-sq2=rev*rev
-
-while sq2 :
-    r=sq2%10
-    rev2=rev2*10+r
-    sq2//=10
-
-if sq==rev2 :
+s=n*n
+st=str(n)
+sq=int(st[::-1])**2
+ns=str(sq)
+if str(s)[::-1]==ns:
     print(True)
-else :
+else:
     print(False)
