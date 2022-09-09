1373d36fb22733c4aa7398ea1ef449422a89b437
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-09-09 12:10:28 +0530

    def is_prime(n):     for i in range(2,int(n**0.5)+1):         if n%i==0:             return "not a prime";     else:         return "prime"; n=int(input()) print(is_prime(n))

--- prime_number.py
+++ prime_number.py
@@ -1,10 +1,8 @@
+def is_prime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return "not a prime";
+    else:
+        return "prime";
 n=int(input())
-flag=0
-if n > 1 :
-    for i in range(2,n) :
-        if n%i==0 :
-            flag=1
-if flag :
-    print("not a prime")
-else :
-    print("prime")
+print(is_prime(n))
