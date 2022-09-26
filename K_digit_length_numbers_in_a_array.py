18cf30966be98936523b6a733ca34dd568d51408
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-09-26 22:59:27 +0530

    n,k=map(int,input().split()) l=list(map(int,input().split())) a=[] for i in l:     if i>=0:         a.append(len(str(i)))     else:         i=-i         a.append(len(str(i))) print(a.count(k))

---
+++ K_digit_length_numbers_in_a_array.py
@@ -0,0 +1,10 @@
+n,k=map(int,input().split())
+l=list(map(int,input().split()))
+a=[]
+for i in l:
+    if i>=0:
+        a.append(len(str(i)))
+    else:
+        i=-i
+        a.append(len(str(i)))
+print(a.count(k))
