c88f52fbf2262a0ff5affc83fb29316751d5e536
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-09-26 22:57:02 +0530

    n=int(input()) l=list(map(int,input().split())) a=[] for i in l:     if i>=0:         a.append(len(str(i)))     else:         i=-i         a.append(len(str(i))) print(*a)

--- count_digits.py
+++ count_digits.py
@@ -2,9 +2,9 @@ n=int(input())
 l=list(map(int,input().split()))
 a=[]
 for i in l:
-    if i<0:
-        i=(-1*i)
-        a.append(int(len(str(i))))
+    if i>=0:
+        a.append(len(str(i)))
     else:
-        a.append(int(len(str(i))))
+        i=-i
+        a.append(len(str(i)))
 print(*a)
