c3f2cea0c6403d743048505abc2fd0f623592019
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-10-07 20:04:10 +0530

    a=list(input().lower().replace(' ','')) b=list(input().lower().replace(' ','')) d=[] for i in a:     if i not in b and i not in d:         d.append(i) for i in b:     if i not in a and i not in d:         d.append(i) print(len(d))

---
+++ Non_common_characters.py
@@ -0,0 +1,10 @@
+a=list(input().lower().replace(' ',''))
+b=list(input().lower().replace(' ',''))
+d=[]
+for i in a:
+    if i not in b and i not in d:
+        d.append(i)
+for i in b:
+    if i not in a and i not in d:
+        d.append(i)
+print(len(d))
