cb40049924805ed8f400c08e4ef228bef6d5f010
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-10-11 22:45:27 +0530

    a=input().split() s=a[len(a)-1] k=min(s) if k and k.lower() in s:     print(k.lower()) else:     print(k)

--- minimum_elemnt_from_a_string.py
+++ minimum_elemnt_from_a_string.py
@@ -1,9 +1,7 @@
-s=input().split()
-res=min(s[-1])
-flag=0
-for i in s[-1]:
-    if ord(res)+32==ord(i):
-        print(i)
-        flag=1
-if flag==0:
-    print(min(s[-1]))
+a=input().split()
+s=a[len(a)-1]
+k=min(s)
+if k and k.lower() in s:
+    print(k.lower())
+else:
+    print(k)
