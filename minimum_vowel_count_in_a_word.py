dc8fe3d70cb05f90a5242c7e8d55021d4907b796
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-10-11 22:15:49 +0530

    s=input().lower().split() a=[] for i in s:     c=0     for j in i:         if j in 'aeiou':             c+=1     a.append(c) print(min(a))

--- minimum_vowel_count_in_a_word.py
+++ minimum_vowel_count_in_a_word.py
@@ -1,10 +1,9 @@
-s=input().split()
-c=0
+s=input().lower().split()
 a=[]
 for i in s:
+    c=0
     for j in i:
-        if j in "aeiouAEIOU":
+        if j in 'aeiou':
             c+=1
     a.append(c)
-    c=0
 print(min(a))
