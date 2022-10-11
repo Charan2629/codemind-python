dc56ad2f9a0fb81c425adfe14eeb63f3dbc6330f
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-10-11 22:13:12 +0530

    s=input().lower().split() a=[] for i in s:     c=0     for j in i:         if j in 'aeiou':             c+=1         a.append(c) print(max(a))

--- maximum_vowel_count.py
+++ maximum_vowel_count.py
@@ -1,10 +1,9 @@
-s=input().split()
-c=0
+s=input().lower().split()
 a=[]
 for i in s:
-    for j in i:
-        if j in "aeiouAEIOU":
-            c+=1
-    a.append(c)
     c=0
+    for j in i:
+        if j in 'aeiou':
+            c+=1
+        a.append(c)
 print(max(a))
