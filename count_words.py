7acf7a6e21780e6d695b6b92fa69c38dab463838
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-10-11 22:30:21 +0530

    s=input().lower().split() c=0 for i in s:     if i[0] in 'aeiou' and i[-1] not in 'aeiou':         c+=1 print(c)

--- count_words.py
+++ count_words.py
@@ -1,6 +1,6 @@
-s=input().split()
+s=input().lower().split()
 c=0
 for i in s:
-    if i[0] in "aeiouAEIOU" and i[-1] not in "aeiouAEIOU":
+    if i[0] in 'aeiou' and i[-1] not in 'aeiou':
         c+=1
 print(c)
