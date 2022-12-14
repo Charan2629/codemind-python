574dd2535113f7a11f0364c42d6cfe227d4c2d42
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-12-14 11:31:27 +0530

    n=int(input()) l=list(map(int,input().split())) a=[] b=[] for i in range(n//2):     a.append(l[i]) for i in range(n//2,n):     b.append(l[i]) print(sum(a)) print(sum(b))

--- sum_of_first_half_and_second_half_in_an_array.py
+++ sum_of_first_half_and_second_half_in_an_array.py
@@ -1,10 +1,10 @@
 n=int(input())
 l=list(map(int,input().split()))
-Sum,Sum1=0,0
-for i in l:
-    if l.index(i)<=l.index(n//2):
-        Sum+=i
-    else:
-        Sum1+=i
-print(Sum)
-print(Sum1)
+a=[]
+b=[]
+for i in range(n//2):
+    a.append(l[i])
+for i in range(n//2,n):
+    b.append(l[i])
+print(sum(a))
+print(sum(b))
